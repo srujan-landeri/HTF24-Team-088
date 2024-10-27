2fce294b6568a1933d089fa1e0df19f4dd93588d
lsrujan <srujanlanderi@gmail.com>
2024-10-27 15:20:34 +0530

    Integrated chatbot

--- client/src/components/ui/AuthNavbar.jsx
+++ client/src/components/ui/AuthNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Bars3Icon } from '@heroicons/react/24/outline'
+import logo from '../../logo.png'
 
 function Navbar() {
 
@@ -8,15 +8,17 @@ function Navbar() {
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                        <a href="#" className="flex gap-2 items-center">
+                            <span className="sr-only">Mandrills</span>
                             <img
                                 alt=""
-                                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                                className="h-8 w-auto"
+                                src={logo}
+                                className="h-16 rounded-full w-auto"
                             />
+                            <h1 className='text-2xl'>Mandrills</h1>
                         </a>
                     </div>
+
                 </nav>
             </header>
         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,15 +10,7 @@ export default function Example() {
         <div className="bg-white">  
             <AuthNavbar />         
             <div className="mx-auto max-w-2xl h-screen flex flex-col justify-center">
-                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                    <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Announcing our next round of funding.{' '}
-                        <a href="#" className="font-semibold text-indigo-600">
-                            <span aria-hidden="true" className="absolute inset-0" />
-                            Read more <span aria-hidden="true">&rarr;</span>
-                        </a>
-                    </div>
-                </div>
+                
                 <div className="text-center">
                     <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
                         Your News, Your Way.
